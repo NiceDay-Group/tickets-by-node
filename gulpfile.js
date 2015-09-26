19bf0f7ba529c8e43f36d0799b719ee4a2d90fbd
Aleksey Krasikov <alekseykrasikov.hk@gmail.com>
2015-09-26 13:57:20 +0300

    Create first test.

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,14 @@
 var gulp = require('gulp');
+var mocha = require('gulp-mocha');
+
+gulp.task('test', () => {
+  process.env.NODE_ENV = 'test';
+  process.env.PORT = 3001;
+  return gulp.src(['./test/**/*.test.js'])
+    .pipe(mocha({
+        require: ['./test/bootstrap/chai.js']
+      }))
+});
 
 gulp.task('default', () => {
   console.log('Gulp is running');
---
+++ test/index.test.js
@@ -0,0 +1,7 @@
+/* global describe, it, expect */
+
+describe('Test', () => {
+  it('should be successful', () => {
+    expect(true).to.be.equal(true);
+  });
+});
