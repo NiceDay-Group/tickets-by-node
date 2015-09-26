69d6a8cc2e76236e58ea6cebb46627a13cbc99bc
Aleksey Krasikov <alekseykrasikov.hk@gmail.com>
2015-09-26 18:55:23 +0300

    Add jscs tasks.

--- gulpfile.js
+++ gulpfile.js
@@ -4,14 +4,15 @@ var mocha = require('gulp-mocha');
 var jsValidate = require('gulp-jsvalidate');
 var jshint = require('gulp-jshint');
 var stylish = require('jshint-stylish');
+var jscs = require('gulp-jscs');
 
-const projectFiles = ['./index.js', './lib/**/*.js', './test/**/*.js'];
+const projectFiles = ['**/*.js', '!node_modules/**/*'];
 
 gulp.task('nodemon', () => {
   nodemon({
     script: 'index.js',
     ext: 'js',
-    ignore: ['*.test.js']
+    ignore: ['*.test.js'],
   });
 });
 
@@ -20,19 +21,31 @@ gulp.task('test', () => {
   process.env.PORT = 3001;
   return gulp.src(['./test/**/*.test.js'])
     .pipe(mocha({
-        require: ['./test/bootstrap/chai.js']
-      }))
+        require: ['./test/bootstrap/chai.js'],
+      }));
 });
 
-gulp.task('validate', function () {
+gulp.task('validate', function() {
   return gulp.src(projectFiles)
-      .pipe(jsValidate());
+    .pipe(jsValidate());
 });
 
 gulp.task('lint', function() {
   return gulp.src(projectFiles)
-      .pipe(jshint())
-      .pipe(jshint.reporter(stylish));
+    .pipe(jshint())
+    .pipe(jshint.reporter(stylish));
+});
+
+gulp.task('jscs', () => {
+  return gulp.src(projectFiles)
+    .pipe(jscs())
+    .pipe(jscs.reporter());
+});
+
+gulp.task('jscs:format', () => {
+  return gulp.src(projectFiles)
+      .pipe(jscs({fix: true}))
+      .pipe(gulp.dest('./'));
 });
 
 gulp.task('default', () => {
