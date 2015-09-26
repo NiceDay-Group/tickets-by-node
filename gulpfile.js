6701cc385f192ce769b7fcfe222ce54b1852d761
Aleksey Krasikov <alekseykrasikov.hk@gmail.com>
2015-09-26 14:12:58 +0300

    Add nodemon.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,14 @@
 var gulp = require('gulp');
+var nodemon = require('gulp-nodemon');
 var mocha = require('gulp-mocha');
+
+gulp.task('nodemon', () => {
+  nodemon({
+    script: 'index.js',
+    ext: 'js',
+    ignore: ['*.test.js']
+  });
+});
 
 gulp.task('test', () => {
   process.env.NODE_ENV = 'test';
