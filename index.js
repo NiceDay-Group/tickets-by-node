79a5dcb8b96704a53c1d56a32af961a9cfa98764
Aleksey Krasikov <alekseykrasikov.hk@gmail.com>
2015-10-07 23:55:08 +0300

    Move logic from index to separate files.

--- index.js
+++ index.js
@@ -1,102 +1,16 @@
 'use strict';
 
-var express = require('express');
-var cors = require('cors');
+var app = require('./lib/init/app');
+var mongoose = require('./lib/init/mongoose');
+var passport = require('./lib/init/passport');
 var config = require('./config');
-var mongoose = require('mongoose');
-var mongooseTypes = require('mongoose-types');
-mongooseTypes.loadTypes(mongoose, 'email');
-
-var passport = require('passport');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var session = require('express-session');
-var User = require('./lib/models/User');
-var localSignUpStrategy = require('./lib/strategies/localSignUp');
-var localSignInStrategy = require('./lib/strategies/localSignIn');
 
 mongoose.connect(config.mongoUrl);
 
-const app = express();
-
-app.use(cors({
-  origin: 'http://localhost:4200',
-  credentials: true
-}));
-
-app.use(cookieParser());
-app.use(bodyParser.json());
-
-passport.serializeUser(function(user, done) {
-  done(null, user.id);
-});
-
-passport.deserializeUser(function(id, done) {
-  User.findById(id, function(err, user) {
-    done(err, user);
-  });
-});
-
-passport.use('local-signup', localSignUpStrategy);
-passport.use('local-signin', localSignInStrategy);
-
-app.use(session({
-  secret: 'secret',
-  cookie: {
-    maxage: 3600000
-  },
-  resave: false,
-  saveUninitialized: true,
-}));
 app.use(passport.initialize());
 app.use(passport.session());
 
-function isLoggedIn(req, res, next) {
-  if (req.isAuthenticated()) {
-    return next();
-  }
-
-  next(new Error(401));
-}
-
-app.get('/', (req, res) => {
-  res.send('Welcome to Tickets.by');
-});
-
-app.get('/protected', isLoggedIn, (req, res) => {
-  res.json({message: 'Have access'});
-});
-
-app.post('/signup',
-  passport.authenticate('local-signup'),
-  function(req, res) {
-    res.json({
-      user: req.user,
-    });
-  }
-);
-
-app.get('/logout', function(req, res) {
-  req.logout();
-  res.send(200);
-});
-
-app.get('/isLogged', function(req, res) {
-  if (req.user) {
-    res.json(req.user);
-  } else {
-    res.send(401);
-  }
-})
-
-app.post('/signin',
-  passport.authenticate('local-signin'),
-  function(req, res) {
-    res.json({
-      user: req.user,
-    });
-  }
-);
+app.use('/api', require('./lib/routes/api'));
 
 app.listen(config.port, () => {
   console.log('Node app is running on port', config.port);
