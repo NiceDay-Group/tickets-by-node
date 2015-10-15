23e5f12502bbebb23c57d6c444987e911cca272b
Aliaksei Krasikau <a.krasikau@gmail.com>
2015-10-15 09:33:19 +0300

    Implement error handling.

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 var app = require('./lib/init/app');
 var mongoose = require('./lib/init/mongoose');
 var passport = require('./lib/init/passport');
+var errorHandler = require('./lib/init/errorHandler');
 var config = require('./config');
 
 mongoose.connect(config.mongoUrl);
@@ -11,6 +12,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.use('/api', require('./lib/routes/api'));
+app.use(errorHandler);
 
 app.listen(config.port, () => {
   console.log('Node app is running on port', config.port);
